171edc20ebcf218650eca236c9b95477b8f0109b
Risman Abdilah <risman.abdilah@frisidea.com>
2020-04-21 00:56:38 +0700

    Initialize Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+// Pages
+import Home from './home';
+import Admin from './admin';
+// Styles
+import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<Router>
+			<div className="App">
+				<div className="Menu">
+					<a href="/" className="Menu-Nav">
+						User
+					</a>
+					<a href="/admin" className="Menu-Nav">
+						Admin
+					</a>
+				</div>
+
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route path="/admin">
+						<Admin />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	)
 }
 
 export default App;
